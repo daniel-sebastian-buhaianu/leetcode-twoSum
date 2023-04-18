cd0f3cb01a600204026d63d849d71a2ed6948403
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-04-18 14:44:01 +0300

    implemented solution

--- main.kt
+++ main.kt
@@ -1,7 +1,31 @@
 fun main() {
-
+    println(twoSum(
+        nums = intArrayOf(0, 3, -3, 4, -1),
+        target = -1
+    ).joinToString())
 }
 
 fun twoSum(nums: IntArray, target: Int): IntArray {
-    
+    // Initialize list of remainders, where:
+    // remainders[i] = target - nums[i]
+    val remainders: MutableList<Int> = mutableListOf()
+
+    // Iterate through nums array
+    var i = 0
+    while (i < nums.size) {
+        // Check if we found a solution
+        val index = remainders.indexOf(nums[i])
+        if (index >= 0) {
+            return intArrayOf(index, i)
+        } 
+
+        // Add new item to remainders
+        remainders.add(target - nums[i])
+
+        // Go to next item in array
+        i++
+    }
+
+    // If it gets here -> no solution found
+    return intArrayOf()
 }
